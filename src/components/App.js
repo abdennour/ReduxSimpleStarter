2c077549eceaf89d4c5a0a2616d7a47322772977
abdennour <mail@abdennoor.com>
2017-08-13 22:29:22 +0300

    git bug fix step-2

---
+++ src/components/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+import { searchYoutube } from '../api';
+class App extends Component {
+  state = { videos: [], selectedVideo: null };
+  videoShape = { snippet: {}, id: {} };
+  componentDidMount() {
+    this.searchVideo('Today news');
+  }
+
+  searchVideo = term => {
+    searchYoutube(term, videos =>
+      this.setState({
+        videos,
+        selectedVideo: videos.length
+          ? { ...videos[0].snippet, ...videos[0].id }
+          : videoShape
+      })
+    );
+  };
+
+  debouncedSearchVideo = () => {
+    return _.debounce(term => {
+      this.searchVideo(term);
+    }, 870);
+  };
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <SearchBar onSearchTermChange={this.debouncedSearchVideo()} />
+        </div>
+        <VideoDetail {...this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
