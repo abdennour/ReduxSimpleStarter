ffb7f4311a48be8b1f7c96ca08d1fd85f200d3d9
abdennour <mail@abdennoor.com>
2017-08-15 01:38:54 +0300

    feat: prepare GoogleMap component

---
+++ src/components/GoogleMap.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: this.props
+    });
+  }
+  render() {
+    return <div className="map" ref="map" />;
+  }
+}
+
+export default GoogleMap;
