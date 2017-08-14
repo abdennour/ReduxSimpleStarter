c30e15028025bce5a576e5d540ba8d7e93035d73
abdennour <mail@abdennoor.com>
2017-08-14 15:30:17 +0300

    refactor: Migrate to create-react-app

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import './index.css';
+import App from './components/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import registerServiceWorker from './registerServiceWorker';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
-  </Provider>
-  , document.querySelector('.container'));
+  </Provider>,
+  document.querySelector('#root')
+);
+registerServiceWorker();
