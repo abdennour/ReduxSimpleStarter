23e35a4be2f13cdd224cd122afdabb81fa032c06
abdennour <mail@abdennoor.com>
2017-08-15 00:31:35 +0300

    feat: connect with Redux DevTools

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
     <App />
   </Provider>,
   document.querySelector('#root')
